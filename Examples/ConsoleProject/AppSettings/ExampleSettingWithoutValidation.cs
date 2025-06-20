cc23a2691f8863a1b2a1e26b6ccb0c0c6b9968f8
Ieuan Walker <walker.ieuan@gmail.com>
2025-06-20 09:11:48 +0100

    Add ConsoleProject with settings and validation examples
    Updated the solution to include a new "ConsoleProject" that references `IeuanWalker.AppSettings` and `IeuanWalker.AppSettings.Generator`. Improved readability in `AppSettingsGenerator.cs` by adding spaces and comments. Created `ConsoleProject.csproj` and added a simple `Program.cs` file. Introduced `ExampleSettingWithoutValidation` and `ExampleSettingWithValidation` classes, with the latter implementing validation rules using FluentValidation.

---
+++ Examples/ConsoleProject/AppSettings/ExampleSettingWithoutValidation.cs
@@ -0,0 +1,9 @@
+﻿using IeuanWalker.AppSettings;
+
+namespace ConsoleProject.AppSettings;
+
+public class ExampleSettingWithoutValidation : IAppSettings
+{
+	public static string? SectionName => "ExampleSetting";
+	public required string Test { get; set; }
+}
